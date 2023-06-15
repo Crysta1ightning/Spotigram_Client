96b03c6e1ac8ef841334f03216acddc712be8a91
CodyChiu <cody_chiu@gapp.nthu.edu.tw>
2023-06-15 18:00:44 +0800

    radio css

--- src/radio/home.jsx
+++ src/radio/home.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import '../App.css'
-import './Radio.scss';
+import './Radio.scss'
+import './Home.scss';
 
 function Home(props) {
   const [friendRadio, setFriendRadio] = useState([
@@ -22,40 +23,60 @@ function Home(props) {
         <h1 class="col-2">電台</h1>
         <div class="col-7"></div>
         <span class="col">
-        <h3 >搜尋</h3>
         <input 
           className="search"
           type = "search" 
-          placeholder = "Search" 
+          placeholder = "ID/電台名稱" 
           onChange = {handleSearch}
-        /></span>
+        />
+        {/* <button type="button" class="btn-search" alt="..."></button> */}
+        <img src="images/icon_search.png" type="button" alt="..."></img>
+        </span>
+
       </div>
+      <div class="row title"><p class="col-3">你的電台</p></div>
       <div class="row">
-        <h3 class="col-2">你的電台</h3>
-        <button class="col-3" onClick={()=>{
+        <div class="col-2"></div>
+        <button type="button" class="col btn-radio col-5 btn-sq-responsive" onClick={()=>{
           props.setPage(1);
-        }}>+</button>
+        }}></button>
+
       </div>
+      <div class="row title"><p class="col-3">好友電台</p></div>
       <div className="friendRadio" class="row">
-        <h3>好友電台</h3>
         {
           friendRadio.map(radio => 
-            <div className="radio inline">
-              <div className="inline">{radio.title}</div>&emsp;
-              <div className="inline">{radio.owner}</div>&emsp;
-              <div className="inline">{radio.viewers}</div>
+            // <div className="radio inline">
+            //   <div className="inline">{radio.title}</div>&emsp;
+            //   <div className="inline">{radio.owner}</div>&emsp;
+            //   <div className="inline">{radio.viewers}</div>
+            // </div>
+            // <div class="radio card col-50" style="width: 15rem;">
+            //     <img src="images/radio_cover.png" class="card-img-top" alt="..."></img>
+            //     <div class="card-body">
+            //         <p class="card-text ">{radio.title}</p>
+            //         <p class="card-text artist">{radio.owner}'s radio</p>
+            //         <p>{radio.viewers}</p>
+            //     </div>
+            // </div>
+            <div className="radio inline col">
+              <img src="images/radio_cover.png" class="card-img-top" alt="..."></img>
+               <p className="card-text inline">{radio.title}</p>&emsp;
+               <p className="inline">{radio.owner}</p>&emsp;
+               <p className="inline">{radio.viewers}</p>
             </div>
           )
         }
       </div>
+      <div class="row title"><p class="col-3">熱門電台</p></div>
       <div className="friendRadio" class="row">
-      <h3>熱門電台</h3>
         {
           friendRadio.map(radio => 
-            <div className="radio inline">
-              <div className="inline">{radio.title}</div>&emsp;
-              <div className="inline">{radio.owner}</div>&emsp;
-              <div className="inline">{radio.viewers}</div>
+            <div className="radio inline col">
+              <img src="images/radio_cover.png" class="card-img-top" alt="..."></img>
+               <p className="card-text inline">{radio.title}</p>&emsp;
+               <p className="inline">{radio.owner}</p>&emsp;
+               <p className="inline">{radio.viewers}</p>
             </div>
           )
         }
