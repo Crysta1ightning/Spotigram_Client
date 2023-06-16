fad1bc5dcef0ae94521f85f159e7f170f2ae2c9a
CodyChiu <cody_chiu@gapp.nthu.edu.tw>
2023-06-16 20:04:55 +0800

    home horizontal scroll

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,35 +7,49 @@ function Home() {
     {id: 0, title: "天是空的", artist: "li3li3", cover:"images/song.png"},
     {id: 1, title: "A song", artist: "li3li3", cover:"images/song1.png"},
     {id: 2, title: "!@#$%^&*(", artist: "li3li3", cover:"images/song.png"},
+    {id: 3, title: "天是空的", artist: "li3li3", cover:"images/song.png"},
+    {id: 4, title: "A song", artist: "li3li3", cover:"images/song1.png"},
+
+  ])
+  const [recommend, setRecommend] = useState([
+    {id: 0, title: "你想一想", artist: "li3li3", cover:"images/song1.png"},
+    {id: 1, title: "B song", artist: "li3li3", cover:"images/song.png"},
+    {id: 2, title: "yo yo", artist: "li3li3", cover:"images/song1.png"},
+    {id: 3, title: "天是空的", artist: "li3li3", cover:"images/song.png"},
+    {id: 4, title: "A song", artist: "li3li3", cover:"images/song1.png"},
+    {id: 5, title: "!@#$%^&*(", artist: "li3li3", cover:"images/song.png"},
+    {id: 6, title: "天是空的", artist: "li3li3", cover:"images/song.png"},
+    {id: 7, title: "A song", artist: "li3li3", cover:"images/song1.png"},
+    {id: 8, title: "!@#$%^&*(", artist: "li3li3", cover:"images/song.png"},
   ])
 
   return (
-    <div className="home">
-      <div class="row" className="story">
-        <p class="h1">Story</p>
+    <div>
+      <div className="row">
+        <p className="h1">Story</p>
 
       </div>
-      <div class="row" className="songs">
-        <p class="h1 col">早安!</p>
-        <div class="col">
+      <div className="row">
+        <p className="h1 row">早安!</p>
+        <div className="scrolling-wrapper">
           {song.map(music => 
-            <div class="card col">
-              <img type="button" src={music.cover} class="card-img-top"></img>
-                  <p class="song">{music.title}</p>
-                  <p class="artist">{music.artist}</p>
+            <div className="card col-2">
+              <img type="button" src={music.cover} className="card-img-top"></img>
+                  <p className="song">{music.title}</p>
+                  <p className="artist">{music.artist}</p>
             </div>
           )}
         </div>
       </div>
       
-      <div class="row" className="songs">
-        <p class="h1 col">美好的明天!</p>
-        <div class="">
-          {song.map(music => 
-            <div class="card col-2">
-              <img type="button" src={music.cover} class="card-img-top"></img>
-                  <p class="song">{music.title}</p>
-                  <p class="artist">{music.artist}</p>
+      <div className="row">
+        <p className="h1 row">美好的明天!</p>
+        <div className="scrolling-wrapper">
+          {recommend.map(music => 
+            <div className="card col-2" >
+              <img type="button" src={music.cover} className="card-img-top"></img>
+                  <p className="card-text song">{music.title}</p>
+                  <p className="card-text artist">{music.artist}</p>
             </div>
           )}
         </div>
