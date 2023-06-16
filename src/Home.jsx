02adef6c62d6e7c0f6b4f3117a62bfd0b9ed247a
CodyChiu <cody_chiu@gapp.nthu.edu.tw>
2023-06-16 15:13:35 +0800

    home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,46 @@
-import { useState } from 'react'
-// import './App.css'
+import { useState } from 'react';
+import './Home.scss';
+import './App.css';
 
 function Home() {
-  const [count, setCount] = useState(0)
+  const [song, setSong] = useState([
+    {id: 0, title: "天是空的", artist: "li3li3", cover:"images/song.png"},
+    {id: 1, title: "A song", artist: "li3li3", cover:"images/song1.png"},
+    {id: 2, title: "!@#$%^&*(", artist: "li3li3", cover:"images/song.png"},
+  ])
 
   return (
-    <>
-      Home1
-    </>
+    <div className="home">
+      <div class="row" className="story">
+        <p class="h1">Story</p>
+
+      </div>
+      <div class="row" className="songs">
+        <p class="h1 col">早安!</p>
+        <div class="col">
+          {song.map(music => 
+            <div class="card col">
+              <img type="button" src={music.cover} class="card-img-top"></img>
+                  <p class="song">{music.title}</p>
+                  <p class="artist">{music.artist}</p>
+            </div>
+          )}
+        </div>
+      </div>
+      
+      <div class="row" className="songs">
+        <p class="h1 col">美好的明天!</p>
+        <div class="">
+          {song.map(music => 
+            <div class="card col-2">
+              <img type="button" src={music.cover} class="card-img-top"></img>
+                  <p class="song">{music.title}</p>
+                  <p class="artist">{music.artist}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
   )
 }
 
