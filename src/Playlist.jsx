cae88fa82d2f1affbfd452a56ab304287b3dfcf4
kelvin <kelvin92831@gmail.com>
2023-06-17 01:43:52 +0800

    playlist fetch

--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
-import './Playlist.css'
+import './Playlist.scss'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,12 +13,35 @@ function App() {
     {id:5, title: "抖音", owners:["li3", "Cody"]}
 
   ])
-  const [MyplaylistSet] = useState([
-    {id:0, title: "周杰倫", owner:"kk4944" },
-    {id:1, title: "成發歌單", owner:"kk4944"},
-    {id:2, title: "抖音", owners:"kk4944"}
+  //const [MyplaylistSet] = useState([
+  //  {id:0, title: "周杰倫", cover:"images/song1.png"},
+  //  {id:1, title: "成發歌單", cover:"images/song1.png"},
+  //  {id:2, title: "抖音", cover:"images/song1.png"}
+  //
+  //])
+
+  const [MyplaylistSet, setPlaylistSet] = useState([
+    {id:0, title: "周杰倫", cover:"images/song1.png"},
+    {id:1, title: "成發歌單", cover:"images/song1.png"},
+    {id:2, title: "抖音", cover:"images/song1.png"}
 
   ])
+  useEffect(() => {
+    fetchPlaylistData();
+  }, [])
+
+  const fetchPlaylistData = async () => {
+    let response = await fetch("http://localhost:3000/api/playlist");
+    let data = await response.json();
+
+    console.log(data);
+    let newplaylist = [];
+    for (let i=0; i<data.length; i++) {
+      newplaylist.push({id: data[i].playlist_id, title: data[i].playlistname, cover:"images/song.png"});
+    }
+    setPlaylistSet(newplaylist);
+    console.log(newplaylist);
+  }
 
   return (
     <div>
@@ -30,7 +53,7 @@ function App() {
           {
             MyplaylistSet.map(playlist=> 
               <div className="playlist inline col-2">
-                <img type="button" src="images/song.png" class="card-img-top" alt="..."></img>
+                <img type="button" src={playlist.cover} className="card-img-top"></img>
                 <p className="inline">{playlist.title}</p>&emsp;
               </div>
             )
