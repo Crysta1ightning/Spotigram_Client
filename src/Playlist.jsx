4b87c117ce2f48f61edc9ec6fa7f3f5d7636df09
nevikw39 <nevikw39@icloud.com>
2023-06-18 22:14:01 +0800

    Format Playlist

--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -5,12 +5,12 @@ import './Playlist.scss'
 function App() {
   const [count, setCount] = useState(0);
   const [ShareplaylistSet] = useState([
-    {id:0, title: "周杰倫", owners:["li3", "Cody"] },
-    {id:1, title: "成發歌單", owners:["li3", "Cody"]},
-    {id:2, title: "抖音", owners:["li3", "Cody"]},
-    {id:3, title: "周杰倫", owners:["li3", "Cody"] },
-    {id:4, title: "成發歌單", owners:["li3", "Cody"]},
-    {id:5, title: "抖音", owners:["li3", "Cody"]}
+    { id: 0, title: "周杰倫", owners: ["li3", "Cody"] },
+    { id: 1, title: "成發歌單", owners: ["li3", "Cody"] },
+    { id: 2, title: "抖音", owners: ["li3", "Cody"] },
+    { id: 3, title: "周杰倫", owners: ["li3", "Cody"] },
+    { id: 4, title: "成發歌單", owners: ["li3", "Cody"] },
+    { id: 5, title: "抖音", owners: ["li3", "Cody"] }
 
   ])
   //const [MyplaylistSet] = useState([
@@ -21,9 +21,9 @@ function App() {
   //])
 
   const [MyplaylistSet, setPlaylistSet] = useState([
-    {id:0, title: "周杰倫", cover:"images/song1.png"},
-    {id:1, title: "成發歌單", cover:"images/song1.png"},
-    {id:2, title: "抖音", cover:"images/song1.png"}
+    { id: 0, title: "周杰倫", cover: "images/song1.png" },
+    { id: 1, title: "成發歌單", cover: "images/song1.png" },
+    { id: 2, title: "抖音", cover: "images/song1.png" }
 
   ])
   useEffect(() => {
@@ -36,8 +36,8 @@ function App() {
 
     console.log(data);
     let newplaylist = [];
-    for (let i=0; i<data.length; i++) {
-      newplaylist.push({id: data[i].playlist_id, title: data[i].playlistname, cover:"images/song.png"});
+    for (let i = 0; i < data.length; i++) {
+      newplaylist.push({ id: data[i].playlist_id, title: data[i].playlistname, cover: "images/song.png" });
     }
     setPlaylistSet(newplaylist);
     console.log(newplaylist);
@@ -47,25 +47,25 @@ function App() {
     <div>
       <div class="title d-flex align-items-start"><h1 class="inline">播放清單</h1></div>
 
-        <div className="row"> 
-          <p class="subtitle inline">個人播放清單</p>
-          <div className="scrolling-wrapper">
+      <div className="row">
+        <p class="subtitle inline">個人播放清單</p>
+        <div className="scrolling-wrapper">
           {
-            MyplaylistSet.map(playlist=> 
+            MyplaylistSet.map(playlist =>
               <div className="card col-2">
                 <img type="button" src={playlist.cover} className="card-img-top"></img>
                 <p className="card-text playlist-title">{playlist.title}</p>&emsp;
               </div>
             )
           }
-          </div>
         </div>
+      </div>
 
-        <div className="row">
-          <p class="subtitle inline">共享播放清單</p>
-          <div className="scrolling-wrapper">
+      <div className="row">
+        <p class="subtitle inline">共享播放清單</p>
+        <div className="scrolling-wrapper">
           {
-            ShareplaylistSet.map(playlist=> 
+            ShareplaylistSet.map(playlist =>
               <div className="card col-2">
                 <img type="button" src="images/song1.png" class="card-img-top"></img>
                 <p className="card-text playlist-title">{playlist.title}</p>
@@ -73,15 +73,15 @@ function App() {
               </div>
             )
           }
-          </div>
         </div>
+      </div>
 
-        <div className='row'>
-          <p className='bind'>u can't see me</p>
-          <p className='bind'>u can't see me</p>
-          <p className='bind'>u can't see me</p>
-        </div>
-      
+      <div className='row'>
+        <p className='bind'>u can't see me</p>
+        <p className='bind'>u can't see me</p>
+        <p className='bind'>u can't see me</p>
+      </div>
+
 
 
 
