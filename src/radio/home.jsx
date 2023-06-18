311656ef4acb74badb72c332c54893e6d1496c5c
CodyChiu <cody_chiu@gapp.nthu.edu.tw>
2023-06-18 11:29:15 +0800

    radio home fix

--- src/radio/home.jsx
+++ src/radio/home.jsx
@@ -18,59 +18,62 @@ function Home(props) {
   }
 
   return (
-<div className="radioPage">
-      <div class="row justify-content-between " id="radioTop">
-        <h1 class="col-2 d-inline-flex">電台</h1>
-        <div class="col-3 justify-content-end">
+  <div className="radioPage">
+      <div className="row justify-content-between " id="radioTop">
+        <h1 className="col-2 d-inline-flex">電台</h1>
+        <div className="col-3 justify-content-end">
         <input 
           className="search"
           type = "search" 
           placeholder = "ID/電台名稱" 
           onChange = {handleSearch}
         />
-        {/* <button type="button" class="btn-search" alt="..."></button> */}
-        <img src="images/icon_search.png" class="" type="button" alt="..."></img>
+        <img src="images/icon_search.png" type="button" alt="..."></img>
         </div>
-
       </div>
-      <div class="row title"><p class="col-2">你的電台</p></div>
-      <div class="row">
-        <div class="col-2">
-          <img type="button" class="card-img-top offset-5" src="images/add_radio.png" onClick={()=>{
+
+      <div className="row">
+      <p className="row title">你的電台</p>
+        <div className="row">
+          <button type="button" className="col-2 btn btn-radio img-fluid" src="images/add_radio.png" onClick={()=>{
             props.setPage(1);
-          }}></img>
+          }}></button>
+          <div className="col">
+            <div className="scrolling-wrapper">
+              {friendRadio.map(radio => 
+                  <div className="radio col-3">
+                    <img type="button" src="images/radio_cover.png" className="card-img-top" alt="..."></img>
+                    <p className="">{radio.title}&emsp;{radio.owner}&emsp;{radio.viewers}</p>
+                  </div>
+                )}
+            </div>
+          </div>
         </div>
-        
-
-      </div>
-      <div class="row title"><p class="col-2">好友電台</p></div>
-      <div className="friendRadio" class="row">
-        {
-          friendRadio.map(radio => 
-
-            <div className="radio inline col">
-              <img type="button" src="images/radio_cover.png" class="card-img-top" alt="..."></img>
-               <p className="inline">{radio.title}</p>&emsp;
-               <p className="inline">{radio.owner}</p>&emsp;
-               <p className="inline">{radio.viewers}</p>
-            </div>
-          )
-        }
-      </div>
-      <div class="row title"><p class="col-3">熱門電台</p></div>
-      <div className="friendRadio" class="row">
-        {
-          friendRadio.map(radio => 
-            <div className="radio inline col">
-              <img type="button" src="images/radio_cover.png" class="card-img-top" alt="..."></img>
-               <p className="inline">{radio.title}</p>&emsp;
-               <p className="inline">{radio.owner}</p>&emsp;
-               <p className="inline">{radio.viewers}</p>
-            </div>
-          )
-        }
       </div>
 
+      <div className="row">
+        <p className="row title">好友電台</p>
+        <div className="scrolling-wrapper">
+          {friendRadio.map(radio => 
+              <div className="radio col-2">
+                <img type="button" src="images/radio_cover.png" className="card-img-top" alt="..."></img>
+                <p className="">{radio.title}&emsp;{radio.owner}&emsp;{radio.viewers}</p>
+              </div>
+            )}
+        </div>
+      </div>
+
+      <div className="row ">
+        <p className="row title ">熱門電台</p>
+        <div className="scrolling-wrapper">
+          {friendRadio.map(radio => 
+              <div className="radio col-2">
+                <img type="button" src="images/radio_cover.png" className="card-img-top" alt="..."></img>
+                <p className="">{radio.title}&emsp;{radio.owner}&emsp;{radio.viewers}</p>
+              </div>
+            )}
+        </div>
+      </div>
       <div className='row'>
           <p className='bind'>u can't see me</p>
           <p className='bind'>u can't see me</p>
