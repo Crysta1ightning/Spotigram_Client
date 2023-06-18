e7c7102771388ff7767bd9faa2997122253ae058
dCo-oCd <cody9647@gmail.com>
2023-06-18 19:35:11 +0800

    sidebar hover change

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { NavLink } from "react-router-dom";
-import './App.css'
+// import './App.css'
+import './Sidebar.scss'
 
 import { MDBListGroup, MDBListGroupItem, MDBNavbarLink, MDBIcon } from 'mdb-react-ui-kit';
 
@@ -10,16 +11,16 @@ function Sidebar() {
     <div className="sidebar-container">
       <h1 className="px-0.5 ">Spotigram</h1>
       <MDBListGroup className='bg-dark text-center'>
-        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/" action noBorders>
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/" action noBorders>
           <MDBIcon fas icon='home' className=''/>Home
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/profile" action noBorders>
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/profile" action noBorders>
           <MDBIcon fas icon='user-circle' />Profile
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/playlist" action noBorders>
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/playlist" action noBorders>
           <MDBIcon fas icon='bars' />Playlists
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/radio" action noBorders>
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/radio" action noBorders>
           <MDBIcon fas icon='broadcast-tower' />Radio
         </MDBListGroupItem>
       </MDBListGroup>
