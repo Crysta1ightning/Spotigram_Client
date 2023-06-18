e4c4cbe8cb8d1946da6379821fdc41d02af21afb
nevikw39 <nevikw39@icloud.com>
2023-06-18 22:36:56 +0800

    Hide Showcase

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -23,9 +23,9 @@ function Sidebar() {
         <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/radio" action noBorders>
           <MDBIcon fas icon='broadcast-tower' />Radio
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/showcase" action noBorders>
+        {/* <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/showcase" action noBorders>
           <MDBIcon fas icon='music' />Showcase
-        </MDBListGroupItem>
+        </MDBListGroupItem> */}
       </MDBListGroup>
     </div>
   )
