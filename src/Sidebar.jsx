0fd766f18a637cc953abab68e0c93c0439c4fb9e
dCo-oCd <cody9647@gmail.com>
2023-06-18 19:27:57 +0800

    sidebar animation fix

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -7,19 +7,19 @@ import { MDBListGroup, MDBListGroupItem, MDBNavbarLink, MDBIcon } from 'mdb-reac
 function Sidebar() {
 
   return (
-    <div>
-      <h1 className="px-0.5">Spotigram</h1>
-      <MDBListGroup className='bg-dark text-center text-white'>
-        <MDBListGroupItem tag={NavLink} className='d-flex justify-content-between align-items-center' to="/" action noBorders>
-          <MDBIcon fas icon='home' />Home
+    <div className="sidebar-container">
+      <h1 className="px-0.5 ">Spotigram</h1>
+      <MDBListGroup className='bg-dark text-center'>
+        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/" action noBorders>
+          <MDBIcon fas icon='home' className=''/>Home
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='d-flex justify-content-between align-items-center' to="/profile" action noBorders>
+        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/profile" action noBorders>
           <MDBIcon fas icon='user-circle' />Profile
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='d-flex justify-content-between align-items-center' to="/playlist" action noBorders>
+        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/playlist" action noBorders>
           <MDBIcon fas icon='bars' />Playlists
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='d-flex justify-content-between align-items-center' to="/radio" action noBorders>
+        <MDBListGroupItem tag={NavLink} className='text-white d-flex justify-content-between align-items-center' to="/radio" action noBorders>
           <MDBIcon fas icon='broadcast-tower' />Radio
         </MDBListGroupItem>
       </MDBListGroup>
