413e48cef97cd238b07483715289beb128b01b34
dCo-oCd <cody9647@gmail.com>
2023-06-19 21:45:02 +0800

    story background

--- src/storybackcolor.jsx
+++ src/storybackcolor.jsx
@@ -1,48 +1,49 @@
-var rgb = getAverageRGB(document.getElementById('i'));
-document.body.style.backgroundColor = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
+// var rgb = getAverageRGB(document.getElementById('i'));
+// document.body.style.backgroundColor = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')';
 
 export function getAverageRGB(imgEl) {
 
-var blockSize = 5, // only visit every 5 pixels
-    defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
-    canvas = document.createElement('canvas'),
-    context = canvas.getContext && canvas.getContext('2d'),
-    data, width, height,
-    i = -4,
-    length,
-    rgb = {r:0,g:0,b:0},
-    count = 0;
-    
-if (!context) {
-    return defaultRGB;
-}
+    // console.log(imgEl);
+    var blockSize = 5, // only visit every 5 pixels
+        defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+        canvas = document.createElement('canvas'),
+        context = canvas.getContext && canvas.getContext('2d'),
+        data, width, height,
+        i = -4,
+        length,
+        rgb = {r:0,g:0,b:0},
+        count = 0;
+        
+    if (!context) {
+        return defaultRGB;
+    }
 
-height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+    height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+    width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
 
-context.drawImage(imgEl, 0, 0);
+    context.drawImage(imgEl, 0, 0);
 
-try {
-    data = context.getImageData(0, 0, width, height);
-} catch(e) {
-    /* security error, img on diff domain */alert('x');
-    return defaultRGB;
-}
+    try {
+        data = context.getImageData(0, 0, width, height);
+    } catch(e) {
+        /* security error, img on diff domain */alert('x');
+        return defaultRGB;
+    }
 
-length = data.data.length;
+    length = data.data.length;
 
-while ( (i += blockSize * 4) < length ) {
-    ++count;
-    rgb.r += data.data[i];
-    rgb.g += data.data[i+1];
-    rgb.b += data.data[i+2];
-}
+    while ( (i += blockSize * 4) < length ) {
+        ++count;
+        rgb.r += data.data[i];
+        rgb.g += data.data[i+1];
+        rgb.b += data.data[i+2];
+    }
 
-// ~~ used to floor values
-rgb.r = ~~(rgb.r/count);
-rgb.g = ~~(rgb.g/count);
-rgb.b = ~~(rgb.b/count);
+    // ~~ used to floor values
+    rgb.r = ~~(rgb.r/count);
+    rgb.g = ~~(rgb.g/count);
+    rgb.b = ~~(rgb.b/count);
 
-return rgb;
+    return rgb;
 
 }
