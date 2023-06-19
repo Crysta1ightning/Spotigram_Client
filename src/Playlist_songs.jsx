52986593e20354bd675a0a85daa5baa42d099c8f
kelvin <kelvin92831@gmail.com>
2023-06-19 20:20:17 +0800

    playlist v3

--- src/Playlist_songs.jsx
+++ src/Playlist_songs.jsx
@@ -4,62 +4,67 @@ import './Playlist_songs.scss'
 
 function Playlistsong() {
     const [cur_playlistID] = useState(2);
-    const [playlist_name, setPlaylistName] = useState(); 
-    const [song, setSong] = useState([]);
-    const [playlistSong, setPlaylistSong] = useState([
-        { id: 0, title: "周杰倫", cover: "images/5.png" },
-        { id: 1, title: "成發歌單", cover: "images/6.png" },
-        { id: 2, title: "抖音", cover: "images/4.jpg" }
-      ])
+    const [playlist_name, setPlaylistName] = useState([]); 
+    const [playlistSong, setPlaylistSong] = useState([]);
       useEffect(() => {
+
+        const fetchPlaylistSongData = async () => {
+          const data = await fetch("http://localhost:3000/api/playlist_song").then(r => r.json());
+          
+          const data1 = await fetch("http://localhost:3000/api/song").then(r => r.json());
+          
+          const playlistdata = await fetch("http://localhost:3000/api/playlist").then(r => r.json());
+  
+          
+          //console.log(data);
+          //console.log(data1);
+          let newplaylistsong = [];
+          for (let i = 0; i < data.length; i++) {
+              if (data[i].playlist_id == cur_playlistID) {
+                  for (let j = 0; j < data1.length; j++){
+                      if (data[i].song_id == data1[j].song_id){
+                          newplaylistsong.push({ id: data1[j].song_id, title: data1[j].songname, artist: data1[j].artist});
+                      }
+                  }
+              }
+          }
+  
+          let newplaylistname = [];
+          for (let i = 0; i < playlistdata.length; i++) {
+            if(playlistdata[i].playlist_id == cur_playlistID){
+              newplaylistname.push({name: playlistdata[i].playlistname});
+            }
+              
+          }
+  
+          setPlaylistName(newplaylistname);
+          console.log(newplaylistname);
+          setPlaylistSong(newplaylistsong);
+          console.log(playlistSong);
+      }
+
         fetchPlaylistSongData();
-        fetchPlaylistName();
       }, [])
     
-      const fetchPlaylistName = async () => {
-        const data = await fetch("http://localhost:3000/api/playlist").then(r => r.json());
 
-        //console.log(data);
-        let newplaylistname = [];
-        for (let i = 0; i < data.length; i++) {
-          if(data[i].playlist_id == cur_playlistID){
-            newplaylistname.push({name: data[i].playlistname});
-          }
-            
-        }
-
-        setPlaylistName(newplaylistname);
-        console.log(newplaylistname);
-      } 
     
-      const fetchPlaylistSongData = async () => {
-        const data = await fetch("http://localhost:3000/api/playlist_song").then(r => r.json());
-        
-        const data1 = await fetch("http://localhost:3000/api/song").then(r => r.json());
-        
-        
-        //console.log(data);
-        //console.log(data1);
-        let newplaylistsong = [];
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].playlist_id == cur_playlistID) {
-                for (let j = 0; j < data1.length; j++){
-                    if (data[i].song_id == data1[j].song_id){
-                        newplaylistsong.push({ id: data1[j].song_id, title: data1[j].songname, artist: data1[j].artist});
-                    }
-                }
-            }
-        }
-        
-        setPlaylistSong(newplaylistsong);
-        console.log(newplaylistsong);
-    }
+
   
   return (
     <div>
-        <div>{playlist_name[0].name}</div>
-        <div>{playlistSong[0].title}</div>
-        <div>{playlistSong[1].title}</div>
+        <div className="title d-flex align-items-start"><h1 className="inline">{playlist_name[0]?.name}</h1></div>
+
+        <div>
+          {
+            playlistSong.map(song =>
+              <div className='row'>
+                <div className='col song'>{song.title}</div>
+                <div className='col song'>{song.artist}</div>
+              </div>
+            )
+          }
+        </div>
+
 
 
         
