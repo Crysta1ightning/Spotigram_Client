ef239f24cad65d26d453be95c7ac2810b36c9a8d
dCo-oCd <cody9647@gmail.com>
2023-06-19 21:06:25 +0800

    sidebar hide text on small screen

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -11,17 +11,17 @@ function Sidebar() {
     <div className="sidebar-container min-vh-100 ">
       <img src='./images/spotigram_title.png' width='150rem' className='mt-3'></img>
       <MDBListGroup className='text-center'>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/" action noBorders>
-          <MDBIcon fas icon='home' />Home
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-lg-flex justify-content-between align-items-center' to="/" action noBorders>
+          <MDBIcon fas icon='home ' /><span className='d-none d-lg-flex'>Home</span>
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/profile" action noBorders>
-          <MDBIcon fas icon='user-circle' />Profile
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-lg-flex justify-content-between align-items-center' to="/profile" action noBorders>
+          <MDBIcon fas icon='user-circle' /><span className='d-none d-lg-flex'>Profile</span>
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/playlist" action noBorders>
-          <MDBIcon fas icon='bars' />Playlists
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-lg-flex justify-content-between align-items-center' to="/playlist" action noBorders>
+          <MDBIcon fas icon='bars' /><span className='d-none d-lg-flex'>Playlists</span>
         </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/radio" action noBorders>
-          <MDBIcon fas icon='broadcast-tower' />Your radio
+        <MDBListGroupItem tag={NavLink} className='bar text-white d-lg-flex justify-content-between align-items-center' to="/radio" action noBorders>
+          <MDBIcon fas icon='broadcast-tower' /><span className='d-none d-lg-flex'>Your radio</span>
         </MDBListGroupItem>
         {/* <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-items-center' to="/showcase" action noBorders>
           <MDBIcon fas icon='music' />Showcase
