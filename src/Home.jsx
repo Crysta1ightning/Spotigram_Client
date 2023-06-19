de57ca59803912492cbfcdd3e6a5f9f89ccc52e0
dCo-oCd <cody9647@gmail.com>
2023-06-19 11:41:44 +0800

    story circle update

--- src/Home.jsx
+++ src/Home.jsx
@@ -141,9 +141,11 @@ function Home() {
         <div className="story-container">
           {story.map((music, index) =>
             <div className="stories col-xl-1 col-4 text-center">
-              <div><img type="button" src={music.pfp} className="img-container mb-2 story-pfp shadow img-fluid rounded-circle" onError={({currentTarget}) => {handlePfpErrored(currentTarget, music.id)}} 
-              data-bs-toggle="modal" data-bs-target="#storyModal" onClick={() => { toChangeStory(index) }}></img>
-              <p className="overflow-hidden">{music.username}</p></div>
+              <span data-bs-toggle="modal" data-bs-target="#storyModal">
+                <img type="button" src={music.pfp} className="img-container mb-2 story-pfp shadow img-fluid rounded-circle" data-bs-toggle="button"
+                onError={({currentTarget}) => {handlePfpErrored(currentTarget, music.id)}} onClick={() => { toChangeStory(index) }}></img>
+              </span>
+              <p className="overflow-hidden">{music.username}</p>
             </div>
           )}
         </div>
