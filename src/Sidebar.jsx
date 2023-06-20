97398475d19f7e921bf7f12ff7e64c9a27cc6222
llshang <lishang0225@gmail.com>
2023-06-20 11:16:15 +0800

    friend's radio label draft

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { NavLink } from "react-router-dom";
 // import './App.css'
 import './Sidebar.scss'
@@ -20,7 +20,45 @@ import {
 
 function Sidebar() {
 
+  const [users, setUsers] = useState([]);
+  const [friends, setFriends] = useState([]);
 
+  let default_userid = JSON.parse(localStorage.getItem('user_id')); // TODO: local storage 
+
+  useEffect(() => {
+    const fetchUsersData = async () => {
+        const data = await fetch("http://localhost:3000/api/user").then(r => r.json());
+
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].user_id === default_userid) {
+                setUsers(data[i]);
+                break;
+            }
+        }
+    }
+
+    fetchUsersData();
+
+    const fetchFriendsData = async () => {
+        const friendsData = await fetch("http://localhost:3000/api/friend?user_id=" + default_userid).then(r => r.json());
+        const usersData = await fetch("http://localhost:3000/api/user").then(r => r.json());
+        let userFriends = [];
+
+        for (let i = 0; i < friendsData.length; i++) {
+            const friendId = (friendsData[i].user1_id == default_userid ? friendsData[i].user2_id : friendsData[i].user1_id);
+            for (let j = 0; j < usersData.length; j++) {
+                if (usersData[j].user_id == friendId) {
+                    userFriends.push({ id: friendId, name: usersData[j].username, pfp: "./images/user" + friendId + ".png" });
+                    break;
+                }
+            }
+        }
+        console.log(userFriends);
+        setFriends(userFriends);
+    }
+
+    fetchFriendsData();
+}, [])
 
   // if (localStorage.getItem("user_id") == null) return (<></>)
   return (
@@ -46,18 +84,17 @@ function Sidebar() {
         <br />
       <p className="text-center mt-5">Friends' Radio</p>
       <MDBListGroup className='friend-block'>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-content-center' noBorders>
-          <img src='./images/user0.png' className='img-fluid rounded-circle' width={45} />
-        </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-content-center' noBorders>
-          <img src='./images/user4.png' className='img-fluid rounded-circle' width={45} />
-        </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-content-center' noBorders>
-          <img src='./images/user2.png' className='img-fluid rounded-circle' width={45} />
-        </MDBListGroupItem>
-        <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-content-center' noBorders>
-          <img src='./images/user3.png' className='img-fluid rounded-circle' width={45} />
-        </MDBListGroupItem>
+        {
+          friends.map((friend) => 
+          <MDBListGroupItem tag={NavLink} className='bar text-white d-flex justify-content-between align-content-center' noBorders>
+            <img src={friend.pfp} className='img-fluid rounded-circle' width={50} />
+            <div className='col px-3'>
+              <div className='friend-name text-right'>{friend.name}</div>
+              <div className='join-label text-right'>→ Join Now</div>
+            </div>
+          </MDBListGroupItem>
+          ) 
+        }
         
       </MDBListGroup>
 
